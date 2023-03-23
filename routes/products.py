a26243ccca3ebc0aa9f5b1e2e48cc21d41d77bda
carlosjorge7 <carlosjorge7@gmail.com>
2023-03-24 00:17:46 +0100

    without async

--- routes/products.py
+++ routes/products.py
@@ -6,27 +6,27 @@ from schemas.schemas import Product
 routerProducts = APIRouter(prefix='/products')
 
 @routerProducts.get('/', response_model = list[Product])
-async def get_products():
+def get_products():
     return conn.execute(products.select()).fetchall()
 
 @routerProducts.post('/', response_model = Product)
-async def create_product(product: Product):
+def create_product(product: Product):
     new_product = {'name': product.name, 'description': product.description}
     res = conn.execute(products.insert().values(new_product))
     return conn.execute(products.select().where(products.c.id == res.lastrowid)).first()
 
 @routerProducts.get('/{id}', response_model = Product)
-async def get_product(id: str):
+def get_product(id: str):
     res = conn.execute(products.select().where(products.c.id == id)).first()
     return res
 
 @routerProducts.delete('/{id}', status_code = 200)
-async def delete_product(id: str):
+def delete_product(id: str):
     conn.execute(products.delete().where(products.c.id == id))
     return Response(status_code=200)
 
 @routerProducts.put('/{id}', response_model = Product)
-async def update_product(id: str, product: Product):
+def update_product(id: str, product: Product):
     conn.execute(products.update()
                         .values(name = product.name, description = product.description)
                         .where(products.c.id == id))
