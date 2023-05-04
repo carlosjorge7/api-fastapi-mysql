79da6402d9b744b7772516024ce65c83fefb87bc
carlosjorge7 <carlosjorge7@gmail.com>
2023-05-04 14:09:08 +0200

    connect tu azure

--- config/db.py
+++ config/db.py
@@ -4,7 +4,13 @@ from config.enviroment import DB_HOST, DB_USER, DB_NAME, DB_PORT, DB_PASSWORD
 # LOCAL
 DATABASE_URL = 'mysql+pymysql://'+DB_USER+':'+DB_PASSWORD+'@'+DB_HOST+':'+DB_PORT+'/'+DB_NAME
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+ssl_args = {
+    "ssl": {
+        "ca": "/home/carlos/Escritorio/api-fastapi-mysql/ca.pem"
+    }
+}
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, connect_args=ssl_args)
 meta = MetaData(bind=engine)
 
 conn = engine.connect()
